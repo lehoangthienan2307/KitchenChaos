c62f975b111c1272009319d136761d609db9dc13
lehoangthienan2307 <vem803825@gmail.com>
2023-06-12 16:49:34 +0700

    Plate UI for icon
    - Add 2d sprite package
    - Add UI icon for kitchen objects in plate

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -37,4 +37,8 @@ public class PlateKitchenObject : KitchenObject
         }
         
     }
+    public List<KitchenObjectSO> GetKitchenObjectSOList()
+    {
+        return kitchenObjectSOList;
+    }
 }
