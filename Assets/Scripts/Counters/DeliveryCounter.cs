9f94891d8beb32b5bd261eb6349709df74af180a
lehoangthienan2307 <vem803825@gmail.com>
2023-06-13 18:26:53 +0700

    Delivery Counter, Shader Graph
    - Add Delivery Counter
    - Make Arror Shader Graph for delivery counter

---
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if (player.HasKitchenObject())
+        {
+            //only accept plate
+            if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+            {
+                player.GetKitchenObject().DestroySelf();
+            }
+        }
+    }
+}
