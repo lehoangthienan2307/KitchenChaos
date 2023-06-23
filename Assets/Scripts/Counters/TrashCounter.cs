c19594c35337da8e6ba296cc97ce7893cf0fb1e3
lehoangthienan2307 <vem803825@gmail.com>
2023-06-23 17:25:33 +0700

    Game pause, clear statics
    - Add Game Pause
    - Clear all statics event

--- Assets/Scripts/Counters/TrashCounter.cs
+++ Assets/Scripts/Counters/TrashCounter.cs
@@ -6,6 +6,10 @@ using System;
 public class TrashCounter : BaseCounter
 {
     public static event EventHandler OnAnyObjectTrashed;
+    new public static void ResetStaticData()
+    {
+        OnAnyObjectTrashed = null;
+    }
     public override void Interact(Player player)
     {
         if (player.HasKitchenObject())
